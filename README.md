9e1381553dee3285c98871f17d496819364df68f
Jurica <jurs.slovinac2@gmail.com>
2022-04-11 20:55:18 +0200

    Web templates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-#Shanosot
+# Shanosot
 
-Shnapsot is real-time chat application made in Flask with emphasis on secure exchange of messages
+### Shnapsot is real-time chat application made in Flask with emphasis on secure exchange of messages
 
-To run this you need to install following modules
-`pip install flask`
+To run this you need to install following modules  
+`pip install flask flask-socketio`
--- website/views.py
+++ website/views.py
@@ -5,3 +5,19 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def index():
     return render_template('index.html')
+
+@views.route('/rooms')
+def rooms():
+    return render_template('chat_rooms.html')
+
+@views.route('/users')
+def users():
+    return render_template('users.html')
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
+
+@views.route('/register')
+def register():
+    return render_template('register.html')
